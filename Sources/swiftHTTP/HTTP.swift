55204509c65d8a4e6e41e0408a2e1d67ccf32bd1
Hannes Harnisch <haharnisch@uni-potsdam.de>
2020-10-12 16:59:04 +0200

    first http class

---
+++ Sources/swiftHTTP/HTTP.swift
@@ -0,0 +1,84 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hannes Harnisch on 12.10.20.
+//
+
+import Foundation
+
+/**
+ Class for handling HTTP Requests
+ */
+class HTTP {
+    private var urlRequest:URLRequest?
+    private let methode:HttpMethode
+    private var session:URLSessionDataTask?
+    init(methode:HttpMethode){
+        self.methode = methode
+    }
+    func with<T:Codable>(url:String,body:T? = nil) -> HTTP? {
+        self.urlRequest = URLRequest(url: URL(string: url)!)
+        self.urlRequest?.httpMethod = self.methode.rawValue
+        switch self.methode {
+        case .get:
+            return self
+        default:
+            guard body != nil else {
+                return nil
+            }
+            self.urlRequest!.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            guard let encoded = try? JSONEncoder().encode(body) else{
+                return nil
+            }
+            self.urlRequest?.httpBody = encoded
+            return self
+        }
+    }
+    func setAuth(type:String,credentials:String) -> HTTP {
+        self.urlRequest!.setValue("\(type) \(credentials)", forHTTPHeaderField: "Authorization")
+        return self
+    }
+    func customHeaders(list:Dictionary<String,String>){
+        for item in list{
+            self.urlRequest!.setValue(item.key, forHTTPHeaderField: item.key)
+        }
+    }
+    func onResult(callback:@escaping (URLResponse?,Result<Data,Error>) -> Void) -> HTTP{
+        self.registerDataTask(callback: callback)
+    }
+    func onResult<D:Decodable>(callback:@escaping (URLResponse?,Result<D,Error>)->Void) -> HTTP{
+        self.registerDataTask { (response, result) in
+            switch result{
+            case .success(let data):
+                do{
+                    let res = try JSONDecoder().decode(D.self, from: data)
+                    callback(response,.success(res))
+                }catch(let err){
+                    callback(response,.failure(err))
+                }
+            case .failure(let err):
+                callback(response,.failure(err))
+            }
+        }
+    }
+    private func registerDataTask(callback:@escaping (URLResponse?,Result<Data,Error>) -> Void) -> HTTP{
+        self.session = URLSession.shared.dataTask(with: self.urlRequest!, completionHandler: { (data, response, error) in
+            guard error == nil else{
+                callback(response,.failure(error))
+                return
+            }
+            callback(response,.success(data!))
+        })
+        return self
+    }
+    func fire(){
+        self.session?.resume()
+    }
+}
+
+public enum HttpMethode:String{
+    case put = "PUT"
+    case post = "POST"
+    case get = "GET"
+}
--- Sources/swiftHTTP/swiftHTTP.swift
+++
@@ -1,3 +0,0 @@
-struct swiftHTTP {
-    var text = "Hello, World!"
-}
